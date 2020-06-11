ba6217e7b8426fb27bf7355ff115a19077043cf7
Silvio <silviolima@gmail.com>
2020-06-11 12:20:21 -0300

    Title passou pro sidebar.

--- app.py
+++ app.py
@@ -14,14 +14,14 @@ def checar_retorno(send_request):
 def main():
     """ RandonForestClassifier - Titanic """
     
-    # Titulo
-    st.title("Classificação - Titanic")
+    ## Titulo
+    st.sidebar.title("-> Classificação - Titanic")
     
-    #st.sidebar.markdown("## Streamlit - Titanic")
+    #st.markdown("## Streamlit - Titanic")
  
     #html_page = """
-    #<div style="background-color:tomato;padding=50px">
-    #    <p style='text-align:center;font-size:50px;font-weight:bold'>Streamlit - Titanic</p>
+    #<div style="background-color:tomato;padding=10px">
+    #    <p style='text-align:center;font-size:20px;font-weight:bold'>Streamlit - Titanic</p>
     #</div>
     #          """
     #st.markdown(html_page, unsafe_allow_html=True)    
@@ -29,20 +29,20 @@ def main():
     image = Image.open("titanic.png")
     st.sidebar.image(image,caption="",use_column_width=True)
 
-    st.sidebar.markdown("#### --> Streamlit") 
+    #st.sidebar.markdown("#### --> Streamlit") 
     st.sidebar.markdown("#### --> RandomForestClassifier (api)")
     st.sidebar.markdown("#### --> Modelo alocado no Heroku")
 
     st.write('<style>div.Widget.row-widget.stRadio > div{flex-direction:row;}</style>', unsafe_allow_html=True)     
 
-    st.markdown("#### Selecione as caracteristicas do passageiro")
+    st.markdown("### Selecione as caracteristicas do passageiro")
     classe = st.radio('Classe',('Primeira Classe ', 'Segunda Classe', 'Terceira Classe'))
     sexo = st.radio('Sexo',('Homem', 'Mulher'))
     embarque = st.radio('Cidade de Embarque',('Cherboug', 'Queenstown', 'Southampton'))
     idade = st.slider('Idade',min_value=1, max_value=80, value=20, step=5)
     passagem = st.slider('Valor da Passagem',min_value=0, max_value=512, value=100, step=10)
-    st.markdown("#### Caracteristicas selecionadas do passageiro")
-    st.write(classe,'---', sexo, '---',embarque,'---',idade,"anos",'---','$$',passagem)
+    #st.markdown("#### Caracteristicas selecionadas do passageiro")
+    st.write('Caracteristicas: '+ classe,'---', sexo, '---',embarque,'---',idade,"anos",'---','$$',passagem)
     
     data = [{'Classe': classe, 'Sexo': sexo, 'Embarque':embarque, 'Idade': idade, 'Passagem': passagem}]
         
