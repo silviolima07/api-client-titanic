5ccb9e1e0ba2701632b4780d987c7f0d1ea3eed0
Silvio <silviolima@gmail.com>
2020-06-11 11:54:00 -0300

    Alterada a posição dos widgets radio para horizontal, atraves de uma linha de estilo html. Isto sera removido no futuro.

--- app.py
+++ app.py
@@ -15,28 +15,33 @@ def main():
     """ RandonForestClassifier - Titanic """
     
     # Titulo
-    #st.title("Streamlit - Titanic")
+    st.title("Classificação - Titanic")
+    
+    #st.sidebar.markdown("## Streamlit - Titanic")
  
-    html_page = """
-    <div style="background-color:tomato;padding=50px">
-        <p style='text-align:center;font-size:50px;font-weight:bold'>Streamlit - Titanic</p>
-    </div>
-              """
-    st.markdown(html_page, unsafe_allow_html=True)    
+    #html_page = """
+    #<div style="background-color:tomato;padding=50px">
+    #    <p style='text-align:center;font-size:50px;font-weight:bold'>Streamlit - Titanic</p>
+    #</div>
+    #          """
+    #st.markdown(html_page, unsafe_allow_html=True)    
 
     image = Image.open("titanic.png")
     st.sidebar.image(image,caption="",use_column_width=True)
-     
-    st.sidebar.markdown("#### * RandomForestClassifier")
-    st.sidebar.markdown("#### * Modelo alocado no Heroku")     
 
-    st.markdown("Selecione as caracteristicas do passageiro")
+    st.sidebar.markdown("#### --> Streamlit") 
+    st.sidebar.markdown("#### --> RandomForestClassifier (api)")
+    st.sidebar.markdown("#### --> Modelo alocado no Heroku")
+
+    st.write('<style>div.Widget.row-widget.stRadio > div{flex-direction:row;}</style>', unsafe_allow_html=True)     
+
+    st.markdown("#### Selecione as caracteristicas do passageiro")
     classe = st.radio('Classe',('Primeira Classe ', 'Segunda Classe', 'Terceira Classe'))
     sexo = st.radio('Sexo',('Homem', 'Mulher'))
     embarque = st.radio('Cidade de Embarque',('Cherboug', 'Queenstown', 'Southampton'))
     idade = st.slider('Idade',min_value=1, max_value=80, value=20, step=5)
     passagem = st.slider('Valor da Passagem',min_value=0, max_value=512, value=100, step=10)
-    st.markdown("## Caracteristicas selecionadas do passageiro")
+    st.markdown("#### Caracteristicas selecionadas do passageiro")
     st.write(classe,'---', sexo, '---',embarque,'---',idade,"anos",'---','$$',passagem)
     
     data = [{'Classe': classe, 'Sexo': sexo, 'Embarque':embarque, 'Idade': idade, 'Passagem': passagem}]
@@ -49,10 +54,12 @@ def main():
       
     # url da api no heroku 
     url = 'https://app-api-001.herokuapp.com/'
-       
-    
+      
+    st.sidebar.markdown(" ") 
+    st.sidebar.markdown("#### Prever o status do passageiro")
+    st.sidebar.markdown(" ")
 
-    if st.button('Submit'):
+    if st.sidebar.button('Submit'):
         bar = st.progress(0)
         for i in range(11):
             bar.progress(i * 10)
@@ -65,14 +72,15 @@ def main():
             st.warning("Houston we have a problem.")
        
         elif send_request.ok:
-            st.markdown('#### De acordo com o modelo:')
+            st.sidebar.markdown('#### Pela previsão do modelo:')
             status = checar_retorno(send_request)
             
+            #st.sidebar.markdown(" ")
             if status == '1':
-                st.title("Sobreviveu")
+                st.sidebar.markdown("## Sobreviveu")
                 st.balloons()
             else:
-                st.title("Morreu")
+                st.sidebar.markdown("## Morreu")
            
 
 
